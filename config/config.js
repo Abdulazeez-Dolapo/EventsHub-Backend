fd59e83be97a3720e556de6d477f25e04df08f23
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2020-04-22 06:24:22 +0100

    a

--- config/config.js
+++ config/config.js
@@ -7,10 +7,14 @@ module.exports = {
 		options: {
 			dialect: process.env.DB_DIALECT || "mysql",
 			host: process.env.DB_HOST,
+			port: process.env.DB_PORT,
 			storage: "./event_mgt.sql",
 		},
 	},
 	jwt: {
 		secret: process.env.JWT_SECRET || "secret",
+	},
+	production: {
+		use_env_variable: "DATABASE_URL",
 	},
 }
